393480589161c6cab914464e2efcd547cd457cca
Johan Ramirez <johan911019@gmail.com>
2021-11-22 20:35:38 -0500

    Se añade una nueva caracteristica para mostrar un mensaje cuando no se encuentre un recurso

--- src/main/java/com/example/backendcrud/controller/UserController.java
+++ src/main/java/com/example/backendcrud/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.backendcrud.controller;
 
 
+import com.example.backendcrud.Exception.ResourceNotFoundException;
 import com.example.backendcrud.model.User;
 import com.example.backendcrud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +27,12 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id){
+        User user = userRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("User does not exist with id: " + id));
+        return ResponseEntity.ok(user);
     }
 }
